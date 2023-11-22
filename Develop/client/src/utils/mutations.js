c8ec26455cff7c1646bcda01a0b6d4c1ce5deb06
starshipog <chayewhitaker14@gmail.com>
2023-11-21 18:43:51 -0600

    added queries and mutations

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,49 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation adduser($name: String!, $email: String!, $password: String!) {
+    adduser(name: $name, email: $email, password: $password) {
+      token
+      user {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+// HOW TO WRITE MUTATIONS FOR SAVE_BOOK? is it similar to this one for ADD_SKILL?
+// try it and see
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($userId: ID!, $book: String!) {
+    saveBook(userId: $userId, book: $book) {
+      _id
+      name
+      books
+    }
+  }
+`;
+
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($book: String!) {
+    removeBook(book: $book) {
+      _id
+      name
+      books
+    }
+  }
+`;
