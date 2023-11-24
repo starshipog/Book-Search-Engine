7f71b5f49837969acb4c83996365565713b7cd0f
starshipog <chayewhitaker14@gmail.com>
2023-11-23 18:19:27 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Books');
 
 module.exports = mongoose.connection;
