381d7b217477aa955d5940c12623e5902fff7e71
starshipog <chayewhitaker14@gmail.com>
2023-11-26 17:26:45 -0600

    fixed user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,8 +13,8 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-  mutation adduser($name: String!, $email: String!, $password: String!) {
-    adduser(name: $name, email: $email, password: $password) {
+  mutation addUser($name: String!, $email: String!, $password: String!) {
+    addUser(name: $name, email: $email, password: $password) {
       token
       user {
         _id
