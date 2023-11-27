0e5f340a83aea51b1903f99d1f3d0a86ac1c72d1
starshipog <chayewhitaker14@gmail.com>
2023-11-27 01:20:19 -0600

    deploying

--- server/server.js
+++ server/server.js
@@ -43,16 +43,16 @@ const startApolloServer = async () => {
   app.use('/graphql', expressMiddleware(server));
 
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.use(express.static(path.join(__dirname, '../client/build')));
+  // }
   
   // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  //   app.get('*', (req, res) => {
-  //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  //   });
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
   // } 
 
   db.once('open', () => {
