f27ad3de421bf69e871802101c227e0978fabc67
starshipog <chayewhitaker14@gmail.com>
2023-11-27 01:00:33 -0600

    pushing

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Books');
+mongoose.connect('mongodb+srv://cmwUser:KeyMaster08@cluster0.bttprgi.mongodb.net/?retryWrites=true&w=majority')
 
 module.exports = mongoose.connection;
